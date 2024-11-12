1f2ff9cde0d35f67e7cc2340d3a4303f2eaa867d
SungKyum Kim <sungkyum1@naver.com>
2024-11-13 02:19:31 +0900

    chore: 프로젝트 업로드

---
+++ src/main/java/com/continuous/backend/ContinuousBackendApplication.java
@@ -0,0 +1,13 @@
+package com.continuous.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContinuousBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/continuous/backend/ContinuousBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuous.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuousBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
