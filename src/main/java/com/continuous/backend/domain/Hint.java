3e64d4326363add35cb662a101f503a8c7b6b3d7
SungKyum Kim <sungkyum1@naver.com>
2024-11-14 21:22:32 +0900

    feat: Hint(힌트) 도메인 구현

---
+++ src/main/java/com/continuous/backend/domain/Hint.java
@@ -0,0 +1,14 @@
+package com.continuous.backend.domain;
+
+public class Hint {
+
+    private final long id;
+    private final String exampleCode;
+    private final String resourceUrl;
+
+    public Hint(long id, String exampleCode, String resourceUrl) {
+        this.id = id;
+        this.exampleCode = exampleCode;
+        this.resourceUrl = resourceUrl;
+    }
+}
