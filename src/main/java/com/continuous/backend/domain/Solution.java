288bd168648f3a56fec7cabfdf448f59b6e859ea
SungKyum Kim <sungkyum1@naver.com>
2024-11-14 16:03:00 +0900

    feat: Solution(답변) 도메인

---
+++ src/main/java/com/continuous/backend/domain/Solution.java
@@ -0,0 +1,39 @@
+package com.continuous.backend.domain;
+
+import java.util.Objects;
+
+import lombok.Getter;
+
+@Getter
+public class Solution {
+
+    private final long id;
+    private final String content;
+
+    public Solution(long id, String content) {
+        validateContent(content);
+        this.id = id;
+        this.content = content;
+    }
+
+    private void validateContent(String content) {
+        if (content == null || content.isBlank() || content.length() > 100) {
+            throw new IllegalArgumentException("답변은 비어있거나 100자를 넘을 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Solution solution = (Solution)o;
+        return id == solution.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
